3df292ea0e999a2587d578ac84985cfa898a622b
Ломакин Андрей Александрович <lomakinaa-43@tinao.obr.mos.ru>
2022-12-26 16:47:34 +0300

    task6

---
+++ PR1/Task06/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+using static System.Console;
+Clear();
+
+WriteLine("Enter num: ");
+int num = int.Parse(ReadLine()!);
+if (num % 2 == 1)
+    {
+        WriteLine("No");
+    }
+    else
+    {
+        WriteLine("Yes");
+    }
