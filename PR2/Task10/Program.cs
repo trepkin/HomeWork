7e9c72cf85783793da839ab7344cbf88089a9e13
Andrey <trepkin@gmail.com>
2022-12-27 09:25:56 +0300

    Task10 firstone

--- PR2/Task10/Program.cs
+++ PR2/Task10/Program.cs
@@ -4,3 +4,17 @@
 // 918 -> 1
 
 
+using static System.Console;
+Clear();
+
+WriteLine("Введите число трехзначное число");
+int num = int.Parse(ReadLine()!);
+
+if (100 <= num && num <= 999)
+    {
+        num = (num / 10) % 10;
+    }
+    else 
+    {
+        WriteLine("Введено не трехзначное число");
+    }
