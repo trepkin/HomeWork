4edb5104224595063b6d6076c53de2c71a6079ee
LomakinaAndrey <trepkin@gmail.com>
2023-01-08 14:47:41 +0300

    all

--- PR2/Task13/Program.cs
+++ PR2/Task13/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 6
+78 -> третьей цифры нет
+32679 -> 6 */
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число");
+int num = int.Parse(ReadLine()!);
+int i = 10;
+
+if (num < 100)
+    {
+        WriteLine("Третей цифры нет");
+        return;
+    }
+    else 
+    {
+        while (( num / i > 0 ) && (num / ( i * 10 ) != 0 ))
+        {
+            i = i * 10;
+        }
+    }
+num = num / (i/100);
+num = num % 10;
+
+
+
+WriteLine(num);
