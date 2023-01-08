81e319148d0419f5c05c371e8175b287d959570e
LomakinaAndrey <trepkin@gmail.com>
2023-01-08 15:08:34 +0300

    Task23 complete

--- PR3/Task23/Program.cs
+++ PR3/Task23/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число N");
+int N = int.Parse(ReadLine()!);
+int i = 1;
+int stepen = 3;
+
+while ( i <= N )
+    {
+        Write($"{Math.Pow(i, stepen)}, ");
+        i++;
+    }
