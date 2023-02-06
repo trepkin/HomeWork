504818c043ef9b26d12af12e7629d274aa603549
unknown <admin@S-2120-S2-0020.tinao.obr.mos.ru>
2023-02-06 15:21:01 +0300

    Task64 complete

--- PR9/Task64/Program.cs
+++ PR9/Task64/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+int j = 8;
+double Fatorial (int n)
+{
+    if (n==1) return 1;
+    else  return n;
+}
+for (int i = j; i != 0; i--)
+{
+    Console.WriteLine(Fatorial(i));
+}
