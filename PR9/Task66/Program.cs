f15a79b2a94286f6cffa491db8c5ce05efe0d10f
unknown <admin@S-2120-S2-0020.tinao.obr.mos.ru>
2023-02-06 15:23:59 +0300

    task66 complete

--- PR9/Task66/Program.cs
+++ PR9/Task66/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+using static System.Console;
+Clear();
+
+Write("Введите число M и число N через пробел: ");
+
+string[] parameters=ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+
+int sum = 0;
+int Fatorial (int a)
+{
+    if (a==1) return 1;
+    else  return a;
+}
+for (int i = int.Parse(parameters[0]); i <= int.Parse(parameters[1]); i++)
+{
+    sum += Fatorial(i);
+}
+WriteLine(sum);
